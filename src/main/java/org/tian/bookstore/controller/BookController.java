4a72e85b779ca753580c499c181bac5e02c479b4
Daniel Tian <danieltian31@yahoo.com>
2020-08-17 23:38:14 -0500

    add delete and put mapping to BookController

--- src/main/java/org/tian/bookstore/controller/BookController.java
+++ src/main/java/org/tian/bookstore/controller/BookController.java
@@ -4,9 +4,11 @@ import org.apache.coyote.http11.Http11AprProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,5 +45,18 @@ public class BookController {
 		
 		return new ResponseEntity<>("Book added", HttpStatus.ACCEPTED);
 	}
+	
+	@PutMapping(value="{id}")
+	@ResponseBody
+	public String updateBookById(@PathVariable long id) {
+		return "working";
+	}
+	
+	@DeleteMapping(value="{id}")
+	@ResponseBody
+	public String deleteBookById(@PathVariable long id) {
+		return "working";
+	}
+	
 
 }
